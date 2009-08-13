1dcbe4460611fdc6bea3dd8fb2a6fa35d5ca0ab0
Ben Burkert <ben@benburkert.com>
2009-08-12 23:29:51 -0700

    Implemented the runtime command.

--- lib/rip/commands/runtime.rb
+++ lib/rip/commands/runtime.rb
@@ -3,7 +3,13 @@ module Rip
     o 'rip runtime COMMAND'
     x 'Commands for managing your ruby runtime.'
     x 'Type rip runtime to see valid options.'
-    def env(options = {}, command = nil, *args)
+    def runtime(options = {}, command = nil, *args)
+      if command && Rip::Runtime.commands.include?(command)
+        ui.puts 'runtime: ' + Rip::Runtime.call(command, *args).to_s
+      else
+        Rip::Runtime.show_help :runtime
+        ui.puts '', "current runtime: #{Rip::Runtime.active}"
+      end
     end
   end
 end
